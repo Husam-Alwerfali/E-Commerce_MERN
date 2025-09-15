dce391f91bd981f422ff49faa4c93d0a5671d33a
Husam <hos.mohamed@out.edu.ly>
2025-09-15 14:51:27 +0200

    CartPage layout and item display

--- frontend/src/context/Cart/CartProvider.tsx
+++ frontend/src/context/Cart/CartProvider.tsx
@@ -33,6 +33,7 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
         })
       );
         setCartItems(cartItemsMapped);
+        setTotalPrice(cart.totalPrice);
 
     };
 
--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,22 +1,47 @@
 import { Box, Container, Typography } from "@mui/material";
-import { use, useEffect, useState } from "react";
-import { useAuth } from "../context/Auth/AuthContext";
 import { useCart } from "../context/Cart/CartContext";
-import { BASE_URL } from "../api/baseUrl";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const CartPage = () => {
-  const { token } = useAuth();
   const { cartItems, totalPrice } = useCart();
-  const [error, setError] = useState(false);
-
-  
 
   return (
-    <Container maxWidth={false} sx={{ mt: 2 }}>
+    <Container fixed maxWidth={false} sx={{ mt: 2 }}>
       <Typography variant="h4">Cart Page</Typography>
       {cartItems.map((item) => (
-        <Box>{item.title}</Box>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ border: "3px solid #f5f5f5", p: 2, borderRadius: 3, mt: 2 }}
+        >
+          <Box display="flex" flexDirection="row" gap={2} alignItems="center">
+            <img src={item.image} width="90" />
+            <Box>
+              <Typography variant="h6">{item.title}</Typography>
+              <Typography>
+                {item.quantity} x {item.price} LYD
+              </Typography>
+              <Button variant="contained">
+                <DeleteIcon />
+              </Button>
+            </Box>
+          </Box>
+          <ButtonGroup variant="contained" aria-label="Basic button group">
+            <Button>-</Button>
+            <Button>+</Button>
+          </ButtonGroup>
+        </Box>
       ))}
+      <Box>
+        <Typography margin={1}
+          variant="h4">
+          Total : {totalPrice} LYD
+        </Typography>
+      </Box>
     </Container>
   );
 };
